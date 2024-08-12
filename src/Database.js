2b0647d7436495f425884673ba695fe3d2738004
KataRenault <turkoglu.kata@gmail.com>
2024-08-12 15:38:28 +0300

    non functional language update

--- src/Database.js
+++ src/Database.js
@@ -8,6 +8,11 @@ import item5Asset from './assets/walkPlaceholder.gif';
     //                                                                                                       //  
     //                                                                                                       //
 
+  const en = 'EN';
+  const tr = 'TR';
+
+
+
   //Stairs
   export const stairs = [
     { x: 400, y: 640, width: 10, height: 10 , stairId: 1, stairPointer: 2},
@@ -24,22 +29,38 @@ import item5Asset from './assets/walkPlaceholder.gif';
 
   //Items
   export const items = [
-    { x: 200, y: 620, width: 30, height: 30, glow: true, text: 'st1', assetIndex: item1Asset},
-    { x:  80, y: 310, width: 30, height: 30, glow: true, text: 'st2', assetIndex: item2Asset},
-    { x: 160, y: 310, width: 30, height: 30, glow: true, text: 'st3', assetIndex: item3Asset},
-    { x: 240, y: 310, width: 30, height: 30, glow: true, text: 'st4', assetIndex: item4Asset},
-    { x: 320, y: 310, width: 30, height: 30, glow: true, text: 'st5', assetIndex: item5Asset},
+    { x: 200, y: 620, width: 30, height: 30, glow: true, text: en + `1`, assetIndex: item1Asset},
+    { x:  80, y: 310, width: 30, height: 30, glow: true, text: en + `2`, assetIndex: item2Asset},
+    { x: 160, y: 310, width: 30, height: 30, glow: true, text: en + `3`, assetIndex: item3Asset},
+    { x: 240, y: 310, width: 30, height: 30, glow: true, text: en + `4`, assetIndex: item4Asset},
+    { x: 320, y: 310, width: 30, height: 30, glow: true, text: en + `5`, assetIndex: item5Asset},
   ];
 
   //Strings
   export const strings = {
-    stdefault: 'default String.',
-    st1: 'This is string 1',
-    st2: 'This is string 2',
-    st3: 'String 3 be like, Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at augue vel enim scelerisque malesuada id ' + 
+    endefault: 'default String.',
+    en1: 'This is string 1',
+    en2: 'This is string 2',
+    en3: 'String 3 be like, Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at augue vel enim scelerisque malesuada id ' + 
     'eu eros. Phasellus at urna libero. Nullam at diam ultrices, gravida massa id, vestibulum nisi. Praesent tempus eget ' + 
     'purus in aliquam. Quisque placerat rutrum lorem ac ullamcorper. In hac habitasse platea dictumst. Ut gravida ullamcorper ' + 
     'pretium. Vestibulum sodales ante tristique volutpat pretium. Suspendisse suscipit libero in eros fermentum efficitur.',
-    st4: 'This is string 4',
-    st5: 'This is string 5',
+    en4: 'This is string 4',
+    en5: 'This is string 5',
+
+
+
+
+
+
+    trdefault: 'default String.',
+    tr1: 'This is string 1',
+    tr2: 'This is string 2',
+    tr3: 'String 3 be like, Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at augue vel enim scelerisque malesuada id ' + 
+    'eu eros. Phasellus at urna libero. Nullam at diam ultrices, gravida massa id, vestibulum nisi. Praesent tempus eget ' + 
+    'purus in aliquam. Quisque placerat rutrum lorem ac ullamcorper. In hac habitasse platea dictumst. Ut gravida ullamcorper ' + 
+    'pretium. Vestibulum sodales ante tristique volutpat pretium. Suspendisse suscipit libero in eros fermentum efficitur.',
+    tr4: 'This is string 4',
+    tr5: 'This is string 5',
+
   }
